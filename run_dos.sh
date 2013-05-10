664561787efba0bdf1bed81393c3810307c52172
rve <jiongrr@gmail.com>
2013-05-10 15:20:01 +0800

    update

--- run_dos.sh
+++ run_dos.sh
@@ -1,9 +1,11 @@
+: << 'END'
 if  [ -z  "$1" ]
 then
-    nasm loader1.asm -o loader.bin
+    #nasm loader1.asm -o loader.bin
 else
-    nasm $1 -o loader.bin
+    #nasm $1 -o loader.bin
 fi
+END
     cp dos-bochsrc  bochsrc
     #dd if=loader.bin of=a.img bs=512 conv=notrunc
     bochs -q
--- run_myos.sh
+++ run_myos.sh
@@ -1,10 +1,12 @@
+: << 'END'
 if  [ -z  "$1" ]
 then
-    nasm loader1.asm -o loader.bin
+    #nasm loader1.asm -o loader.bin
 else
 
-    nasm $1 -o loader.bin
+    #nasm $1 -o loader.bin
 fi
+END
     cp myos-bochsrc  bochsrc
     #dd if=loader.bin of=a.img bs=512 conv=notrunc
-    bochs -q
+    bochs -q &
